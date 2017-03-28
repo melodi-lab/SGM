f15a00b57201410dbe79fd686390cd2d3e6d0db8
baiwenruo@gmail.com <baiwenruo@gmail.com>
2017-03-28 00:47:09 +0000

    .

---
+++ sourceCodes/python/runFilterMS2.py
@@ -0,0 +1,5 @@
+import os
+
+python filter_spectra.py -z -i ../../../ms2data/malaria/v07539_UofF_malaria_TMT_1.ms2 -c 2 ../data/ms2file/1-ch2.ms2
+
+
--- sourceCodes/python/runTideindex.sh
+++ sourceCodes/python/runTideindex.sh
@@ -6,16 +6,16 @@ rm -rf malaria-index
 	--mods-spec C+57.0214,K+229.16293 --nterm-peptide-mods-spec X+229.16293 \
 	--max-mods 0 \
 	--custom-enzyme '[K]|[X]' \
-	    $TARGETS malaria-index
+	    $TARGETS tide-index
 }
 #CRUX="/n/trombone/s1/wrbai/codes/run_crux/crux-2.1.Linux.x86_64/bin/crux"
-CRUX="/homes/wrbai/crux/crux-2.1.Linux.x86_64/bin/crux"
-TARGETS="/n/trombone/s1/wrbai/codes/msgfplus/jeffH/plasmo_Pfalciparum3D7_NCBI.fasta"
+CRUX="../crux/crux-2.1.Linux.x86_64/bin/crux"
+TARGETS="../../../ms2data/malaria/plasmo_Pfalciparum3D7_NCBI.fasta"
 #TARGETS="/n/trombone/s1/wrbai/codes/msgfplus/jeffH/test.fasta"
 #P_TARGETS="/n/trombone/s1/wrbai/codes/msgfplus/jeffH/plasmo_Pfalciparum3D7_NCBI_dig.fasta"
-OUTPUT="malaria-output"
+OUTPUT="../data/tide-output"
 #for i in 0 1 2 3 4 5 6 7 8 
 #do
-crux_index 8 
+crux_index 2 
 #done
 
