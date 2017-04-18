69cfa0dd8f4d56606ab6d100eacf99cfa1be42ae
baiwenruo@gmail.com <baiwenruo@gmail.com>
2017-04-18 02:14:13 +0000

    .

---
+++ sourceCodes/MSGFPlus/run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function parse {
+    ./parseMsgfPlus.py --msgfPlusTargetFile $1 \
+	--msgfPlusDecoyFile $2 \
+	--spectra $3 \
+	--charge $4 \
+	--ident $5
+}
+
+function runMsgf {
+    $MSGFPlus \
+	-s $1 \
+	-d $2 \
+	-o $3 \
+	-t 50ppm \
+	-tda 0 \
+	-n 1 \
+	-minCharge $4 \
+	-maxCharge $4 \
+	-m 1 \
+	-inst 1 \
+	-e 1 \
+	-ti 0,0 \
+	-minLength 7 \
+	-mod tmtModFile.txt \
+	-thread 1 \
+	-ntt 2 \
+	-ti 0,0 \
+	-ignoreMetCleavage 1 
+	#> $3.log.txt
+	#-mod msgf-mods.txt \
+
+    $MzIDToTSV -i $3 \
+        -o $5 
+}
+
+MSGFPlusDir=$PWD
+
+
+MSGFPlus="java -Xmx3500M -jar $MSGFPlusDir/MSGFPlus.jar"
+MzIDToTSV="java -cp $MSGFPlusDir/MSGFPlus.jar edu.ucsd.msjava.ui.MzIDToTsv"
+
+TARGETDB="/data/tcbbData/Plasmodium/tide-index.peptides.target.fasta"
+DECOYDB="/data/tcbbData/Plasmodium/tide-index.peptides.decoy.fasta"
+
+	id=$1
+	for(( c=2; c<=5; c++))
+	do
+	
+	    CHARGE=$c
+	    outDir="plasm-$id/plasm-ch$CHARGE"
+	echo $outDir
+	    if [[ ! -d $outDir ]]
+	    then
+		mkdir -p $outDir
+	    else
+		rm -rf $outDir
+		mkdir -p $outDir
+	    fi
+	
+		MS2="/data/tcbbData/Plasmodium/plasm-$id-ch$c.mgf"
+		echo $MS2
+	# start with targets
+	    OUTTARGETMZID="$outDir/plasm-ch$CHARGE-targets.mzid"
+	    OUTTARGETTSV="$outDir/plasm-ch$CHARGE-targets.tsv"
+	    runMsgf $MS2 $TARGETDB $OUTTARGETMZID $CHARGE $OUTTARGETTSV
+	
+	# next decoys
+	    OUTDECOYMZID="$outDir/plasm-ch$CHARGE-decoys.mzid"
+	    OUTDECOYTSV="$outDir/plasm-ch$CHARGE-decoys.tsv"
+	    runMsgf $MS2 $DECOYDB $OUTDECOYMZID $CHARGE $OUTDECOYTSV
+	
+	done
